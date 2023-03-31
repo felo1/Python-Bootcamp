9bc0bfd7eb21a41e38a0ed8d79d39b88b453aba7
felo1 <felipe.bazignan@gmail.com>
2023-03-31 17:07:37 -0300

    commit 31-3 dia

--- 5-3 individual/logica.js
+++ 5-3 individual/logica.js
@@ -1,37 +1,41 @@
 document.addEventListener('DOMContentLoaded', (event) => {
- 
 
+document.onload = normal();
 
+function normal(){ 
+document.getElementById("luzazul").style.backgroundColor = "green";  
 
+setTimeout(() => {
+  document.getElementById("luzroja").style.backgroundColor = "black";
+  document.getElementById("luzamarilla").style.backgroundColor = "yellow";  
+  document.getElementById("luzazul").style.backgroundColor = "black";
+  setTimeout(() => { 
+    document.getElementById("luzroja").style.backgroundColor = "red";
+    document.getElementById("luzamarilla").style.backgroundColor = "black";  
+    document.getElementById("luzazul").style.backgroundColor = "black";
+    setTimeout(() => { 
+      document.getElementById("luzroja").style.backgroundColor = "black";
+  }, 3000);
+  }, 1000);
+  }, 3000);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+//* FIN DOCUMENTO ONLOAD */
 })
+/*
+document.addEventListener("mousemove", function(event) {
+
+  // Calculate the distance between the mouse position and the target div
+  const luz = document.getElementById("luzamarilla");
+  const luzFigurita = document.getElementById("figurita");
+
+  const distancia = Math.sqrt(
+    Math.pow(event.pageX - luz.offsetLeft, 2) +
+    Math.pow(event.pageY - luz.offsetTop, 2)
+  );
+
+  if (distancia < 200) {
+    console.log("ya close");
+  }
+});
+*/
