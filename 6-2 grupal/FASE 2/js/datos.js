61ac3d5903f163cb5490babe44ecfef67aa90016
Felo <felipe.bazignan@gmail.com>
2023-04-03 01:21:23 -0400

    avances menores en casa 2-4

--- 6-2 grupal/FASE 2/js/datos.js
+++ 6-2 grupal/FASE 2/js/datos.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     var dataSet = [
         {
           codigo: "1",
-          imagen: "",
           nombre: "Zapato casual",
           descripcion: "Para toda ocasión",
           precio: 10990,
@@ -11,7 +10,6 @@ $(document).ready(function () {
         },
         {
           codigo: "2",
-          imagen: "",
           nombre: "Zapato escolar",
           descripcion: "Sencillo y funcional",
           precio: 19990,
@@ -19,7 +17,6 @@ $(document).ready(function () {
         },
         {
           codigo: "3",
-          imagen: "",
           nombre: "Zapato negro",
           descripcion: "Elegante y cómodo",
           precio: 15000,
@@ -27,7 +24,6 @@ $(document).ready(function () {
         },
         {
           codigo: "4",
-          imagen: "",
           nombre: "Chalabota",
           descripcion: "Lo último en mal diseño",
           precio: 12990,
@@ -35,7 +31,6 @@ $(document).ready(function () {
         },
         {
           codigo: "5",
-          imagen: "",
           nombre: "Bototos",
           descripcion: "De toda la vida",
           precio: 45000,
@@ -43,7 +38,6 @@ $(document).ready(function () {
         },
         {
           codigo: "6",
-          imagen: "",
           nombre: "Tacos",
           descripcion: "Estilo, pero a qué costo?",
           precio: 69000,
@@ -51,7 +45,6 @@ $(document).ready(function () {
         },
         {
           codigo: "7",
-          imagen: "",
           nombre: "Zapato con plataforma",
           descripcion: "14 cm de gracia",
           precio: 12990,
@@ -59,7 +52,6 @@ $(document).ready(function () {
         },
         {
           codigo: "8",
-          imagen: "",
           nombre: "Zapatilla",
           descripcion: "Para hacer deporte",
           precio: 25990,
@@ -67,15 +59,13 @@ $(document).ready(function () {
         },
         {
           codigo: "9",
-          imagen: "",
           nombre: "Chalas zico",
-          descripcion: "Pporque lo vales",
+          descripcion: "Porque lo vales",
           precio: 25990,
           preciomayor: 23391
         },
         {
           codigo: "10",
-          imagen: "",
           nombre: "Zancos",
           descripcion: "Para gente de 1.5m",
           precio: 25990,
@@ -89,22 +79,11 @@ $(document).ready(function () {
         
         { title: "Codigo", data: "codigo" },
         { title: "Nombre", data: "nombre" },
-        { title: "Imagen", data: "imagen" },
         { title: "Descripcion", data: "descripcion" },
         { title: "Precio", data: "precio" },
         { title: "PrecioxMayor", data: "preciomayor" },
-        {
-          title: "En carrito",
-          data: null,
-          render: function (data, type, row) {
-              return ('<button class="agregar">+</button>' +
-              '<span class="contador'+ row.codigo+'"></span>' + //notese la manera de introducir js ahi dentro y referenciar el row
-              '<button class="quitar">-</button>'
-              );
-          }}
       ],
     });
-
 
   });
 
