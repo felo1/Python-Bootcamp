41c4085afb366e79df695e5a90ecff77591e9062
Felo <felipe.bazignan@gmail.com>
2023-04-04 21:19:17 -0400

    inicio mod 3

---
+++ Modulo 3/3-1-1/main.py
@@ -0,0 +1,14 @@
+"""
+comentario
+"""
+
+#comentario
+
+mi_variable = "Anibal"
+#las variables en pithon parece usar ese tipo de case
+mi_edad = 33
+
+nombre = input("ingrese su nombre")
+print("hola" +nombre)
+
+
