529fee8a97d2863a8742d32e7d881bdec5c788b1
Felo <felipe.bazignan@gmail.com>
2023-04-14 22:23:24 -0400

    avance en clases

--- Modulo 3/3-1-4 grupal/main.py
+++ Modulo 3/3-1-4 grupal/main.py
@@ -19,11 +19,43 @@ Esto se debe reflejar en el stock de cada producto.
 #condicional de c/10 compras, agregar 50 más del item, sumarlo a lo entregado por proveedores (stock limitado)
 
 import random
+import time
 
+stock_productos = {"Densimetro nuclear": 120, "Lámparas de sal": 150,}
+stock_proveedor = {"Densimetro nuclear": 150, "Lámparas de sal": 150,}
+def main():
+    contador_compras = 0
+    while(True):
+        numero_productos =  random.randint(1, 10)
+        producto_seleccionado = random.choice(list(stock_productos.keys()))
+        print(f"nos entró una compra de {numero_productos} {producto_seleccionado}\n")
+        if(stock_productos[producto_seleccionado]) >= numero_productos:
+            stock_productos[producto_seleccionado] -=  numero_productos
+            for producto in (stock_productos.keys()):
+                print(f"quedan {stock_productos[producto]} {producto}")
+            #print(f"quedan {stock_productos['Densimetro nuclear']} densimetros")
+            #print(f"quedan {stock_productos['Lámparas de sal']} lamparas")
+            contador_compras += 1
+            if(contador_compras % 10==0):
+                print(f"Nos van quedando {stock_productos[producto_seleccionado]} del producto {producto_seleccionado}")
+            if stock_productos[producto_seleccionado]<=100 and stock_proveedor[producto_seleccionado] >=50: 
+                stock_productos[producto_seleccionado]+=50
+                stock_proveedor[producto_seleccionado]-=50
+            if stock_productos[producto_seleccionado] == 0:
+                print(f"No quedan más unidades del producto {producto_seleccionado}")
+                #time.sleep(3)
+                continue
+            if any(stock_productos) == 0:
+                break
+            #time.sleep(3)
+        else:
+            print("No hay suficiente stock para completar esa compra")
+            print("final:\n")
+            print(f"quedan {stock_productos['Densimetro nuclear']} densimetros")
+            print(f"quedan {stock_productos['Lámparas de sal']} lamparas")       
+            if all(value == 0 for value in stock_productos.values()):
+                break
+            continue
 
-
-stock = {"Densimetro nuclear": 120, "Lámparas de sal": 150,}
-while(true):
-
-
+main()
 #deliberadamente sin break
