5049bf12a41cdb93c628abf1e9cb7da31a98f361
felo1 <felipe.bazignan@gmail.com>
2023-06-20 15:25:19 -0400

    tarea 6-5i completa

--- Modulo 6/6-4i/prueba/app/views.py
+++ Modulo 6/6-4i/prueba/app/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, permission_required
 # Create your views here.
+from django.contrib import messages
 
 def index(request):
  
@@ -15,7 +16,7 @@ def index(request):
 #acá estoy al cargar mi index solicitando cargar en la variable users todos mis usuarios desde la BD
 #para luegos pasarselos al contexto (en una variable llamada igualmente users) para ser renderizada.
 #se agrega render para indicar que debe renderizar el template indicado
-@login_required(login_url="login")
+@permission_required('Administradores', login_url='index', raise_exception=True)
 def forms(request):
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
@@ -30,9 +31,11 @@ def forms(request):
     context = {'form': form}
     return render(request, 'formulario.html', context=context)
 
-@login_required(login_url="login")
+@permission_required('Administradores', login_url='index', raise_exception=True) #esto nos permite tener
+#un nivel adicional de restricciones.
+#TODO: Que no te mande un 403 si no que te devuelva al index pero con un mensaje
 def usuarios(request):
-    users = User.objects.all()
-    context = {"usuarios": users}
-    return render(request, "usuarios.html", {'users': users})
+        users = User.objects.all()
+        context = {"usuarios": users}
+        return render(request, "usuarios.html", {'users': users})
     
